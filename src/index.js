dce0ae3ef34772614bd91977e6c170ef208aa867
Wookeun Song <wookeun.s@gmail.com>
2019-11-05 11:37:50 -0600

    Have you heard of updog?

--- src/index.js
+++ src/index.js
@@ -1,12 +1,81 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
+let IMAGE_ID = 3842
+const IMAGE_URL = `https://randopic.herokuapp.com/images/${IMAGE_ID}`
+const LIKE_URL = `https://randopic.herokuapp.com/likes/`
+const COMMENT_URL = `https://randopic.herokuapp.com/comments/`
 
-  let imageId = 1 //Enter the id from the fetched image here
+document.addEventListener('DOMContentLoaded', main)
 
-  const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
+function main() {
+  fetchImage()
+  addEventListeners()
+}
 
-  const likeURL = `https://randopic.herokuapp.com/likes/`
+function fetchImage() {
+  fetch(IMAGE_URL)
+  .then(response => response.json())
+  .then(image => renderImage(image))
+}
 
-  const commentsURL = `https://randopic.herokuapp.com/comments/`
+function renderImage(image) {
+  let image_card = document.querySelector("#image_card")
+    image_card.querySelector("#image").src = image.url
+    image_card.querySelector("#name").innerText = image.name
+    image_card.querySelector("#likes").innerText = image.like_count
 
-})
+    let commentBox = image_card.querySelector("#comments")
+    for (comment of image.comments) {
+      let li = document.createElement("li")
+      li.innerText = comment.content
+      commentBox.appendChild(li)
+    }
+}
+
+function addEventListeners() {
+  document.querySelector("#like_button").addEventListener("click", addLike)
+  document.querySelector("#comment_form").addEventListener("submit", addComment)
+}
+
+function addLike(e) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "image_id": IMAGE_ID
+    })
+  }
+  fetch(COMMENT_URL, configObj)
+  .then(response => response.json())
+  .then(function() {
+    // Front End Update
+    let likes = e.target.parentElement.querySelector("#likes")
+    likes.innerText = parseInt(likes.innerText, 10) + 1
+  })
+}
+
+function addComment(e) {
+  e.preventDefault()
+
+  let comment = document.createElement("li")
+  comment.innerText = e.target.querySelector("#comment_input").value
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "image_id": IMAGE_ID,
+      "content": comment.innerText
+    })
+  }
+  fetch(COMMENT_URL, configObj)
+  .then(response => response.json())
+  .then(function() {
+    // Front End Update
+    document.querySelector("#comments").appendChild(comment)
+  })
+}
